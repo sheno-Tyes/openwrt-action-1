6667e1539ed4ea4302a1af154e7b394708f8ac92
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2021-11-30 18:32:42 +0800

    Update diy-dockerman-1.sh

--- diy-dockerman-1.sh
+++ diy-dockerman-1.sh
@@ -8,5 +8,4 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-sed -i "/other/d" feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
